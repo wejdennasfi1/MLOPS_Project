8410ee4f80d4c96a76fe840d1c90b86c3c6c330e
wejden <nasfiwejden18@gmail.com>
2025-02-21 15:43:04 +0100

    better SVM  metrics

--- main.py
+++ main.py
@@ -7,19 +7,17 @@ def main():
     parser.add_argument("--train", action="store_true", help="Entraîner le modèle")
     parser.add_argument("--evaluate", action="store_true", help="Évaluer le modèle")
     args = parser.parse_args()
-
-  
-
+    
     if args.prepare:
         X_train, X_test, y_train, y_test, scaler = prepare_data()
         print("Données préparées.")
-
+    
     if args.train:
         X_train, X_test, y_train, y_test, scaler = prepare_data()
         model = train_model(X_train, y_train)
         save_model(model, "svm_model.joblib")
         print("Modèle entraîné et sauvegardé.")
-
+    
     if args.evaluate:
         X_train, X_test, y_train, y_test, scaler = prepare_data()
         model = load_model("svm_model.joblib")
